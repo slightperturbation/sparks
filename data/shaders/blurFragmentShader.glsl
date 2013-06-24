96f98850faaf10e79c209eebd56f633291dbca27
Brian F. Allen <vector@acm.org>
2013-06-23 23:56:40 -0400

    Added blur shaders Renamed Spark.hpp to DBMSpark.hpp Ignore ext

---
+++ data/shaders/blurFragmentShader.glsl
@@ -0,0 +1,46 @@
+#version 150
+
+// Standard output.  See ShaderManager::reloadShader()
+out vec4 outColor;
+
+// From vertex shader
+in vec4 f_fragColor; // interpolated color of fragment from vertex colors 
+in vec2 f_texCoord;  // texture coordinate of vertex
+in vec2 f_blurTexCoords[14]; // Gaussian sample points
+
+//////////////////////////////////////////////////////////////////////
+// Common Uniforms (see RenderCommand)
+uniform mat4  u_projViewModelMat;     // projection * view * model
+uniform mat4  u_viewModelMat;         // transforms object into camera(eye) space
+uniform mat4  u_inverseViewModelMat;  // inverse of the model-view matrix, can give camera position
+uniform mat4  u_projMat;              // projects camera(eye) space to clip(screen) space
+uniform mat3  u_normalMat;            // transpose(inverse(viewModelMat))
+uniform float u_time;                 // current time (in seconds) 
+uniform vec2  u_targetSizeInPixels;   // size in pixels of the current render target
+//////////////////////////////////////////////////////////////////////
+
+uniform vec4 u_color = vec4( 1, 1, 1, 1 );
+
+uniform sampler2D s_color;
+
+void main()
+{
+    outColor = vec4(0.0);
+    outColor += texture(s_color, f_blurTexCoords[ 0])*0.0044299121055113265;
+    outColor += texture(s_color, f_blurTexCoords[ 1])*0.00895781211794;
+    outColor += texture(s_color, f_blurTexCoords[ 2])*0.0215963866053;
+    outColor += texture(s_color, f_blurTexCoords[ 3])*0.0443683338718;
+    outColor += texture(s_color, f_blurTexCoords[ 4])*0.0776744219933;
+    outColor += texture(s_color, f_blurTexCoords[ 5])*0.115876621105;
+    outColor += texture(s_color, f_blurTexCoords[ 6])*0.147308056121;
+    outColor += texture(s_color, f_texCoord         )*0.159576912161;
+    outColor += texture(s_color, f_blurTexCoords[ 7])*0.147308056121;
+    outColor += texture(s_color, f_blurTexCoords[ 8])*0.115876621105;
+    outColor += texture(s_color, f_blurTexCoords[ 9])*0.0776744219933;
+    outColor += texture(s_color, f_blurTexCoords[10])*0.0443683338718;
+    outColor += texture(s_color, f_blurTexCoords[11])*0.0215963866053;
+    outColor += texture(s_color, f_blurTexCoords[12])*0.00895781211794;
+    outColor += texture(s_color, f_blurTexCoords[13])*0.0044299121055113265;
+    // outColor = texture(s_color, f_texCoord         );
+    // outColor *= u_color;
+}
---
+++ data/shaders/blurHortVertexShader.glsl
@@ -0,0 +1,54 @@
+#version 150
+
+// MeshVertex attributes (see Mesh.hpp)
+in vec3 v_position;
+in vec3 v_normal;
+in vec4 v_color;
+in vec3 v_texCoord;
+
+//////////////////////////////////////////////////////////////////////
+// Common Uniforms (see RenderCommand)
+uniform mat4  u_projViewModelMat;     // projection * view * model
+uniform mat4  u_viewModelMat;         // transforms object into camera(eye) space
+uniform mat4  u_inverseViewModelMat;  // inverse of the model-view matrix, can give camera position
+uniform mat4  u_projMat;              // projects camera(eye) space to clip(screen) space
+uniform mat3  u_normalMat;            // transpose(inverse(viewModelMat))
+uniform float u_time;                 // current time (in seconds) 
+uniform vec2  u_targetSizeInPixels;   // size in pixels of the current render target
+//////////////////////////////////////////////////////////////////////
+
+// Out to fragment shader
+out vec4 f_fragColor;      // interpolated color of fragment from vertex colors 
+out vec2 f_texCoord;       // texture coordinate of vertex
+out vec4 f_vertex_screen;  // Projected vertex into the clip-space
+out vec4 f_normal_camera;  // For phong lighting
+out vec4 f_vertex_camera;  // For phong lighting
+
+out vec2 f_blurTexCoords[14];
+
+void main()
+{
+	f_normal_camera = vec4( u_normalMat * v_normal, 0.0 ); // dir
+	f_vertex_camera = u_viewModelMat * vec4( v_position, 1.0 ); // point
+    f_fragColor = v_color ;
+    f_texCoord = vec2( v_texCoord.s, 1 - v_texCoord.t );// flip tex coord to orient vs render
+    f_vertex_screen = u_projViewModelMat * vec4( v_position, 1.0 );
+
+    float scale = 1.0 / u_targetSizeInPixels[1];
+    f_blurTexCoords[ 0] = f_texCoord + vec2(-7*scale, 0.0);
+    f_blurTexCoords[ 1] = f_texCoord + vec2(-6*scale, 0.0);
+    f_blurTexCoords[ 2] = f_texCoord + vec2(-5*scale, 0.0);
+    f_blurTexCoords[ 3] = f_texCoord + vec2(-4*scale, 0.0);
+    f_blurTexCoords[ 4] = f_texCoord + vec2(-3*scale, 0.0);
+    f_blurTexCoords[ 5] = f_texCoord + vec2(-2*scale, 0.0);
+    f_blurTexCoords[ 6] = f_texCoord + vec2(-1*scale, 0.0);
+    f_blurTexCoords[ 7] = f_texCoord + vec2( 1*scale, 0.0);
+    f_blurTexCoords[ 8] = f_texCoord + vec2( 2*scale, 0.0);
+    f_blurTexCoords[ 9] = f_texCoord + vec2( 3*scale, 0.0);
+    f_blurTexCoords[10] = f_texCoord + vec2( 4*scale, 0.0);
+    f_blurTexCoords[11] = f_texCoord + vec2( 5*scale, 0.0);
+    f_blurTexCoords[12] = f_texCoord + vec2( 6*scale, 0.0);
+    f_blurTexCoords[13] = f_texCoord + vec2( 7*scale, 0.0);
+
+    gl_Position = f_vertex_screen;
+}
