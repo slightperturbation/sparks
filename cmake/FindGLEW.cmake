e2fbb2a71237cca69231264a88fc562bd05df476
Brian F. Allen <vector@acm.org>
2013-07-28 23:11:47 -0400

    Ported to GLFW 3.01 (from 2), and added freetype-gl

--- cmake/FindGLEW.cmake
+++ cmake/FindGLEW.cmake
@@ -6,24 +6,51 @@
 # GLEW_INCLUDE_DIR
 # GLEW_LIBRARY
 # 
+# First look for GLEW_ROOT_DIR
 # On windows use environment variable GLEW_ROOT_DIR
+
+SET(GLEW_FOUND "NO")
+
 IF(WIN32)
+
 	FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
-		$ENV{PROGRAMFILES}/GLEW/include
-		${GLEW_ROOT_DIR}/include
-		$ENV{GLEW_ROOT_DIR}/include
+		PATHS
+			${GLEW_ROOT_DIR}
+			$ENV{GLEW_ROOT_DIR}
+		PATH_SUFFIXES
+			include
 		DOC "The directory where GL/glew.h resides")
 
-    FIND_LIBRARY( GLEW_LIBRARY
-        NAMES glew32s glew GLEW glew32 
-        PATHS
-        $ENV{PROGRAMFILES}/GLEW/lib
-        ${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
-        ${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
-		${GLEW_ROOT_DIR}/lib
-        $ENV{GLEW_ROOT_DIR}/lib
-        DOC "The GLEW library")
+    FIND_LIBRARY( GLEW_RELEASE_LIBRARY
+        NAMES glew64s  # Only accept the 64-bit, static build 
+		PATHS
+			${GLEW_ROOT_DIR}
+		    $ENV{GLEW_ROOT_DIR}/lib
+        PATH_SUFFIXES
+			lib 
+			libs
+			bin
+		DOC "The GLEW Release library")
+	set( GLEW_LIBRARY ${GLEW_RELEASE_LIBRARY} )
+
+    FIND_LIBRARY( GLEW_DEBUG_LIBRARY
+        NAMES glew64sd  # Only accept the 64-bit, static build 
+		PATHS
+			${GLEW_ROOT_DIR}
+		    $ENV{GLEW_ROOT_DIR}/lib
+        PATH_SUFFIXES
+			lib 
+			libs
+			bin
+        DOC "The GLEW Debug library")
+
+        IF( GLEW_INCLUDE_DIR AND GLEW_RELEASE_LIBRARY AND GLEW_DEBUG_LIBRARY )
+			SET(GLEW_FOUND "YES")
+			message(STATUS "Found GLEW.")
+		ENDIF( GLEW_INCLUDE_DIR AND GLEW_RELEASE_LIBRARY AND GLEW_DEBUG_LIBRARY )
+
 ELSE(WIN32)
+
 	FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
 		/usr/include
 		/usr/local/include
@@ -46,11 +73,12 @@ ELSE(WIN32)
 		${GLEW_ROOT_DIR}/lib
 		$ENV{GLEW_ROOT_DIR}/lib
 		DOC "The GLEW library")
+
+	IF(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+		SET(GLEW_LIBRARIES ${GLEW_LIBRARY})
+		SET(GLEW_FOUND "YES")
+	    message(STATUS "Found GLEW.")
+	ENDIF(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+
 ENDIF(WIN32)
 
-SET(GLEW_FOUND "NO")
-IF(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-	SET(GLEW_LIBRARIES ${GLEW_LIBRARY})
-	SET(GLEW_FOUND "YES")
-    message(STATUS "Found GLEW.")
-ENDIF(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
