0082b89e111ab5aad17ecea609f0972318a49c0c
Brian F. Allen <vector@acm.org>
2013-09-11 20:40:53 -0400

    Added global namespace output operators for glm classes

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -564,6 +564,36 @@ std::ostream& spark::operator<<( std::ostream& out, const glm::mat4& m )
     return out;
 }
 
+///////////////////////////////////////////////////////////////////
+
+
+std::ostream& operator<<( std::ostream& out, const glm::vec2& v )
+{
+    out << v.x << ", " << v.y;
+    return out;
+}
+std::ostream& operator<<( std::ostream& out, const glm::vec3& v )
+{
+    out << v.x << ", " << v.y << ", " << v.z ;
+    return out;
+}
+std::ostream& operator<<( std::ostream& out, const glm::vec4& v )
+{
+    out << v.x << ", " << v.y << ", " << v.z << ", " << v.w;
+    return out;
+}
+std::ostream& operator<<( std::ostream& out, const glm::mat3& m )
+{
+    out << glm::to_string(m);
+    return out;
+}
+std::ostream& operator<<( std::ostream& out, const glm::mat4& m )
+{
+    out << glm::to_string(m);
+    return out;
+}
+
+
 void 
 spark
 ::checkOpenGLErrors( const char* aCodeStatement, 
