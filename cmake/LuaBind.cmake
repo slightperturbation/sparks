5b203c84b6e8a33f6a096459372dcb71b9a82c1d
Brian F. Allen <vector@acm.org>
2013-11-13 10:34:22 -0500

    Use HomeBrew Lua 5.2 on OSX

--- cmake/LuaBind.cmake
+++ cmake/LuaBind.cmake
@@ -5,68 +5,72 @@
 #
 ########################################################################
 # Lua
-set( LUA_RELEASE 5.2.2 )
-set( LUA_DIR "${CMAKE_SOURCE_DIR}/ext/lua-${LUA_RELEASE}" )
-SET( LUA_SRC_DIR "${LUA_DIR}/src/lua/src" )
-if( EXISTS ${LUA_SRC_DIR}/lapi.c )
-    SET( LUA_SRC
-        ${LUA_SRC_DIR}/lapi.c
-        ${LUA_SRC_DIR}/lauxlib.c
-        ${LUA_SRC_DIR}/lbaselib.c
-        ${LUA_SRC_DIR}/lbitlib.c
-        ${LUA_SRC_DIR}/lcode.c
-        ${LUA_SRC_DIR}/lcorolib.c
-        ${LUA_SRC_DIR}/lctype.c
-        ${LUA_SRC_DIR}/ldblib.c
-        ${LUA_SRC_DIR}/ldebug.c
-        ${LUA_SRC_DIR}/ldo.c
-        ${LUA_SRC_DIR}/ldump.c
-        ${LUA_SRC_DIR}/lfunc.c
-        ${LUA_SRC_DIR}/lgc.c
-        ${LUA_SRC_DIR}/linit.c
-        ${LUA_SRC_DIR}/liolib.c
-        ${LUA_SRC_DIR}/llex.c
-        ${LUA_SRC_DIR}/lmathlib.c
-        ${LUA_SRC_DIR}/lmem.c
-        ${LUA_SRC_DIR}/loadlib.c
-        ${LUA_SRC_DIR}/lobject.c
-        ${LUA_SRC_DIR}/lopcodes.c
-        ${LUA_SRC_DIR}/loslib.c
-        ${LUA_SRC_DIR}/lparser.c
-        ${LUA_SRC_DIR}/lstate.c
-        ${LUA_SRC_DIR}/lstring.c
-        ${LUA_SRC_DIR}/lstrlib.c
-        ${LUA_SRC_DIR}/ltable.c
-        ${LUA_SRC_DIR}/ltablib.c
-        ${LUA_SRC_DIR}/ltm.c
-        # ${LUA_SRC_DIR}/lua.c -- Interpreter
-        # ${LUA_SRC_DIR}/luac.c -- Compiler
-        ${LUA_SRC_DIR}/lundump.c
-        ${LUA_SRC_DIR}/lvm.c
-        ${LUA_SRC_DIR}/lzio.c
-    )
-else()
-    ExternalProject_Add( 
-        lua
-        URL "http://www.lua.org/ftp/lua-${LUA_RELEASE}.tar.gz"
-        PREFIX "${LUA_DIR}"
-        UPDATE_COMMAND ""
-        CONFIGURE_COMMAND ""
-        BUILD_COMMAND ""
-        INSTALL_COMMAND ""
-    )
-    MESSAGE( "\n\n!!!NOTE: Lua not found.  Build project to download, then re-run cmake.\n\n" )
-endif()
+if( APPLE )
+    add_definitions( -DLUA_COMPAT_ALL -DLUA_USE_MACOSX )
+    #add_definitions( -DLUA_USE_MACOSX )
+    set( LUA_LIBRARIES readline lua )
+    SET( LUA_SRC "" )
+    INCLUDE_DIRECTORIES( /usr/local/include )
+else( APPLE )
+    set( LUA_RELEASE 5.2.2 )
+    set( LUA_DIR "${CMAKE_SOURCE_DIR}/ext/lua-${LUA_RELEASE}" )
+    SET( LUA_SRC_DIR "${LUA_DIR}/src/lua/src" )
+    if( EXISTS ${LUA_SRC_DIR}/lapi.c )
+        SET( LUA_SRC
+            ${LUA_SRC_DIR}/lapi.c
+            ${LUA_SRC_DIR}/lauxlib.c
+            ${LUA_SRC_DIR}/lbaselib.c
+            ${LUA_SRC_DIR}/lbitlib.c
+            ${LUA_SRC_DIR}/lcode.c
+            ${LUA_SRC_DIR}/lcorolib.c
+            ${LUA_SRC_DIR}/lctype.c
+            ${LUA_SRC_DIR}/ldblib.c
+            ${LUA_SRC_DIR}/ldebug.c
+            ${LUA_SRC_DIR}/ldo.c
+            ${LUA_SRC_DIR}/ldump.c
+            ${LUA_SRC_DIR}/lfunc.c
+            ${LUA_SRC_DIR}/lgc.c
+            ${LUA_SRC_DIR}/linit.c
+            ${LUA_SRC_DIR}/liolib.c
+            ${LUA_SRC_DIR}/llex.c
+            ${LUA_SRC_DIR}/lmathlib.c
+            ${LUA_SRC_DIR}/lmem.c
+            ${LUA_SRC_DIR}/loadlib.c
+            ${LUA_SRC_DIR}/lobject.c
+            ${LUA_SRC_DIR}/lopcodes.c
+            ${LUA_SRC_DIR}/loslib.c
+            ${LUA_SRC_DIR}/lparser.c
+            ${LUA_SRC_DIR}/lstate.c
+            ${LUA_SRC_DIR}/lstring.c
+            ${LUA_SRC_DIR}/lstrlib.c
+            ${LUA_SRC_DIR}/ltable.c
+            ${LUA_SRC_DIR}/ltablib.c
+            ${LUA_SRC_DIR}/ltm.c
+            # ${LUA_SRC_DIR}/lua.c -- Interpreter
+            # ${LUA_SRC_DIR}/luac.c -- Compiler
+            ${LUA_SRC_DIR}/lundump.c
+            ${LUA_SRC_DIR}/lvm.c
+            ${LUA_SRC_DIR}/lzio.c
+        )
+    else()
+        ExternalProject_Add( 
+            lua
+            URL "http://www.lua.org/ftp/lua-${LUA_RELEASE}.tar.gz"
+            PREFIX "${LUA_DIR}"
+            UPDATE_COMMAND ""
+            CONFIGURE_COMMAND ""
+            BUILD_COMMAND ""
+            INSTALL_COMMAND ""
+        )
+        MESSAGE( "\n\n!!!NOTE: Lua not found.  Build project to download, then re-run cmake.\n\n" )
+    endif()
+    SET( LUA_INCLUDE_DIR "${LUA_SRC_DIR}" ) 
+    INCLUDE_DIRECTORIES( "${LUA_INCLUDE_DIR}" )
+endif( APPLE )
+
 # CFLAGS, extracted from lua's src/Makefile
 #add_definitions( -DLUA_COMPAT_ALL )
-if( WIN32 )
-elseif( APPLE )
-     add_definitions( -DLUA_COMPAT_ALL -DLUA_USE_MACOSX )
-    #add_definitions( -DLUA_USE_MACOSX )
-    set( LUA_LIBRARIES readline )
-endif()
-SET( LUA_INCLUDE_DIR "${LUA_SRC_DIR}" ) 
-INCLUDE_DIRECTORIES( "${LUA_INCLUDE_DIR}" )
+
 ########################################################################
 
 ########################################################################
